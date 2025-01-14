ac7f234b50e1f9c05078b5545c53f28c23d25135
Ryo Onaka <s2381110@bene.fit.ac.jp>
2025-01-14 21:07:51 +0900

    change file name

--- it17.fjct.fit.ac.jp/kanrityumon.php
+++ it17.fjct.fit.ac.jp/kanrityumon.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION['user_id'])){
-  header('Location: index.html');
+  header('Location: index.php');
   exit;
 }
 ?>
--- it17.fjct.fit.ac.jp/logout.php
+++ it17.fjct.fit.ac.jp/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: index.html"); // ログインページにリダイレクト
+header("Location: index.php"); // ログインページにリダイレクト
 exit();
 ?>
