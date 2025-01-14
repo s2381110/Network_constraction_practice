4bb7c0f3744837c743b9f7333fde186c9325742c
Ryo Onaka <s2381110@bene.fit.ac.jp>
2025-01-14 16:09:38 +0900

    delete login form for php

--- it17.fjct.fit.ac.jp/login.php
+++ it17.fjct.fit.ac.jp/login.php
@@ -54,10 +54,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <?php if (isset($error)): ?>
     <p><?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8'); ?></p>
   <?php endif; ?>
-  <form action="login.php" method="post">
-    <input type="text" name="username" placeholder="ユーザー名">
-    <input type="password" name="password" placeholder="パスワード">
-    <button type="submit">ログイン</button>
-  </form>
 </body>
 </html>
