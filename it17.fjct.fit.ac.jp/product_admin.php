aa483fe92789a068d016a313cb5bf73feefd97cb
Ryo Onaka <s2381110@bene.fit.ac.jp>
2025-01-15 20:31:38 +0900

    add product manager

---
+++ it17.fjct.fit.ac.jp/product_admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>商品管理</title>
+  <link rel=\"stylesheet\" href=\"admin_style.css\">
+</head>
+<body>
+
+<div id=\"container\">
+
+  <h1>商品管理</h1>
+
+  <ul>
+    <li><a href=\"product_list.php\">商品一覧</a></li>
+    <li><a href=\"product_add.php\">商品登録</a></li>
+    <li><a href=\"product_edit.php\">商品編集</a></li>
+    <li><a href=\"product_delete.php\">商品削除</a></li>
+  </ul>
+
+</div>
+
+</body>
+</html>
